cbc7bb1714f077ce437ed8320d1f2c538b3d9fa0
Abhishek Makwana <abhi.mak4@gmail.com>
2026-04-24 01:24:23 -0400

    Fix GalaxyBackground import

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
+import GalaxyBackground from "./components/GalaxyBackground";
 
 function App() {
   useEffect(() => {
