8064c152536d1e5282fa73021548bb4d4e93bc8e
Abhishek Makwana <abhi.mak4@gmail.com>
2026-04-24 03:49:57 -0400

    Scrollbar issue

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,11 @@ import GalaxyPro from "./components/GalaxyPro";
 
 function App() {
   useEffect(() => {
-    const glow1 = document.querySelector(".glow1");
+    useEffect(() => {
+  document.body.style.overflowX = "hidden";
+}, []);
+
+const glow1 = document.querySelector(".glow1");
     const glow2 = document.querySelector(".glow2");
 
     const handleMouseMove = (e) => {
